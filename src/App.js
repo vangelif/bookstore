e6e4894b571eb773d6b1a9e674b3d184fa8f2e67
vangelif <ev.fotiades@yahoo.com>
2023-06-13 12:05:41 +0300

    build reducers and actions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
-import React from 'react';
+import React, { Provider } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Categories from './Components/Categories';
 import Books from './Components/Books';
 import Navbar from './Components/Navbar';
+import store from './redux/store';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="Categories" element={<Categories />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="Categories" element={<Categories />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
