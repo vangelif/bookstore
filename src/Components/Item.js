2ee55dcf9926512e264b3ec9adb88c14b6110c61
vangelif <ev.fotiades@yahoo.com>
2023-06-15 18:59:19 +0300

    refactor components to add and remove book

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/booksSlice';
+import { deleteBook } from '../redux/books/booksSlice';
 
 // eslint-disable-next-line object-curly-newline
 const Item = ({ title, author, category, itemId }) => {
   const dispatch = useDispatch();
+  const handleRemoveBook = (id) => () => {
+    dispatch(deleteBook(id));
+  };
 
   return (
     <li>
       <h2>{title}</h2>
       <p>{author}</p>
       <p>{category}</p>
-      <button type="submit" onClick={() => dispatch(removeBook(itemId))}>
+      <button type="submit" onClick={handleRemoveBook(itemId)}>
         Remove
       </button>
     </li>
